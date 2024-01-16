dde6e2f687054ac8440e2ab9d52416a8f5e63afe
AsafBenAsulin <AsafBenAsulin@gmail.com>
2024-01-16 16:22:38 +0200

    Added Footer,css files moved title to shared and home page

---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../Components/Shared/Title'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Title title='Home Page'/>
+        HomePage
+    </div>
+  )
+}
+
+export default HomePage
