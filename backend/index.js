f5c4d0ceb411079696fe27d4a974b671c730de9d
AsafBenAsulin <AsafBenAsulin@gmail.com>
2024-01-16 15:32:39 +0200

    added product schema and connection to mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import User from "./models/User.js";
+import dotenv from "dotenv"
 
 const app = express();
+dotenv.config();
 
 app.use(cors());//dose nothing at the moment
 app.use(express.json());//parses JSONs
-app.use(express.urlencoded({extended:false}));//this is common practice for urlencoded
+app.use(express.urlencoded({ extended: false }));//this is common practice for urlencoded
 
-//routes
+const PORT = process.env.PORT || 8080
+const CONNECTION_STRING = process.env.MONGO_CoNNECTION_STRING
 
-app.listen(8080,function(){
-    console.log("listening to port 8080")
-})
+mongoose.connect(CONNECTION_STRING).then(
+    () => {
+        app.listen(PORT, function () {
+            console.log("listening to port " + PORT);
+        })
+    }).catch(err => { console.log(err.message); });
