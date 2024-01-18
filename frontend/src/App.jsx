77c8fd2e9b2954128532e55bea59980816006f86
AsafBenAsulin <AsafBenAsulin@gmail.com>
2024-01-18 11:33:28 +0200

    Header and search box, imported fa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import HomePage from './Pages/HomePage.jsx'
 import Footer from './Components/Shared/Footer'
+import Header from './Components/Shared/Header.jsx'
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className='d-flex flex-column side-allPage min-width'>
-        {/* <Header/> */}
+        <Header/>
         <main>
-          <Container className='mt-3'>
+          <Container>
             <Routes>
               <Route path="/" element={<HomePage />}></Route>
             </Routes>
