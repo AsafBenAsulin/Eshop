535c65533e298fb27e163e2fa132edafd9e89802
AsafBenAsulin <AsafBenAsulin@gmail.com>
2024-01-18 22:24:09 +0200

    Added actions, added default base url to axios

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -5,21 +5,20 @@ import axios from 'axios'
 import Loading from '../Components/Shared/Loading.jsx';
 import MessageBox from '../Components/Shared/MessageBox.jsx';
 import Products from '../Components/HomePage/Products.jsx';
-
+import { GET_FAIL,GET_REQUEST,GET_SUCCESS } from '../Actions.jsx';
 
 const initialState = { loading: true, error: '', data: [] };
 
 const HomePage = () => {
-  const [state, dispatch] = useReducer(homePageReducer, initialState)
-  const { loading, error, data } = state;
+  const [{ loading, error, data }, dispatch] = useReducer(homePageReducer, initialState);
   useEffect(() => {
     const getProducts = async () => {
-      dispatch({ type: 'GET_REQUEST' });
+      dispatch({ type: GET_REQUEST });
       try {
-        const { data } = await axios.get("http://localhost:8080/api/v1/product");
-        dispatch({ type: 'GET_SUCCESS', payload: data });
+        const { data } = await axios.get("/api/v1/product");
+        dispatch({ type: GET_SUCCESS, payload: data });
       } catch (error) {
-        dispatch({ type: 'GET_FAIL', payload: error.message });
+        dispatch({ type: GET_FAIL, payload: error.message });
       }
     }
     getProducts();
