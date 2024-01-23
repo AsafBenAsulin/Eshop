b3206d6775129bb0f73f58a560710b38ebc90f74
AsafBenAsulin <AsafBenAsulin@gmail.com>
2024-01-23 10:59:52 +0200

    added sign in page, and route to it in app js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container'
 import HomePage from './Pages/HomePage.jsx'
 import Footer from './Components/Shared/Footer'
 import Header from './Components/Shared/Header.jsx'
+import SignIn from './Pages/SignIn.Jsx'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
           <Container>
             <Routes>
               <Route path="/" element={<HomePage />}></Route>
+              <Route path="/signin" element={<SignIn />}></Route>
             </Routes>
           </Container>
         </main>
