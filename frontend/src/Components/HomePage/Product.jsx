81db9522c4696cb0216946dbbb6946a730551c85
AsafBenAsulin <AsafBenAsulin@gmail.com>
2024-01-23 15:53:49 +0200

    GroundWork for cart

--- frontend/src/Components/HomePage/Product.jsx
+++ frontend/src/Components/HomePage/Product.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import Card from 'react-bootstrap/Card'
 import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Rating from '../Shared/Rating';
+import { Store } from '../../store';
 
 
 
 const Product = ({ product }) => {
+    const {state,dispatch:ctxDispatch} = useContext(Store)
+    const {cartItems} = state.cart
     return (
         <Card className='product-card'>
             <Link to={`/product/${product.token}`}>
@@ -20,7 +23,7 @@ const Product = ({ product }) => {
                 <Rating rating={product.rating.rate} numReviews={product.rating.count} />
                 <Card.Text>${product.price}</Card.Text>
                 {product.countInStock === 0 ? <Button variant='light' disabled>Out of stock</Button> :
-                    <Button className='btn-primary'>Add to cart</Button>
+                    <Button className='btn-primary' onClick={()=>{addToCartHandler(product,cartItems,ctxDispatch)}}>Add to cart</Button>
                 }
             </Card.Body>
         </Card>
