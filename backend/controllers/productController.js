9a34c631b94c9dc98f30638e88f600fc7e1e50f2
AsafBenAsulin <AsafBenAsulin@gmail.com>
2024-02-06 15:30:45 +0200

    Finished search in backend  and front

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -33,4 +33,53 @@ const getCategories = async (req, res) => {
   res.send(categories);
 }
 
-export { productData, getProductById, getProductByToken, getCategories }
+const getProductByQuery = async (req, res) => {
+  const { query } = req;
+  const page = query.page || 1;
+  const category = query.category || "";
+  const order = query.order || "";
+  const price = query.price || "";
+  const rating = query.rating || "";
+  const searchQuery = query.query || "";
+  const pageSize = query.pageSize || 6;
+
+  const queryFilter = searchQuery && searchQuery !== "all" ? {
+    title: {
+      $regex: searchQuery,
+      $options: "i"
+    },
+  } : {};
+
+  const categoryFilter = category && category !== "all" ? { category } : {};
+  const priceFilter = price && price !== "all" ? {
+    price: {
+      $gte: Number(price.split("-")[0]),
+      $lte: Number(price.split("-")[1])
+    }
+  } : {};
+  const ratingFilter = rating && rating !== "all" ? {
+    "rating.rate": {
+      $gte: Number(rating)
+    }
+  } : {};
+  const sortOrderFilter = order === "lowest" ? { price: 1 } :
+    order === "highest" ? { price: -1 } :
+      order === "toprated" ? { rating: -1 } :
+        order === "newest" ? { createdAt: -1 } : { _id: -1 };
+
+  const products = await Product.find(
+    { ...queryFilter, ...categoryFilter, ...priceFilter, ...ratingFilter }).sort(
+      sortOrderFilter).skip(
+        pageSize * (page - 1)).limit(pageSize);
+
+  const countProducts = await Product.countDocuments({
+    ...queryFilter, ...categoryFilter, ...priceFilter, ...ratingFilter
+  })
+  res.send({
+    products,countProducts,page,pages:Math.ceil(countProducts/pageSize)
+  })
+}
+
+
+
+export { productData, getProductById, getProductByToken, getCategories, getProductByQuery }
--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -2,44 +2,51 @@ import React, { useEffect, useReducer, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import searchPageReducer from '../Reducers/searchPageReducer';
 import axios from 'axios';
-import { toast } from '../imports';
-import { getError } from '../utils';
+import { Row, toast, Link, Col, Button, LinkContainer } from '../imports';
+import Title from "../Components/Shared/Title"
+import Rating from "../Components/Shared/Rating";
+import Loading from "../Components/Shared/Loading";
+import MessageBox from "../Components/Shared/MessageBox";
+import Product from "../Components/HomePage/Product"
+import { getError, getFilterURI } from '../utils';
 import { GET_REQUEST, GET_SUCCESS, GET_FAIL } from '../Actions';
 
 
 const prices = [
     { name: "$1-$50", value: "1-50" },
     { name: "$51-$200", value: "51-200" },
-    { name: "$201-$1000", value: "201-1000" }];
+    { name: "$201-$1000", value: "201-1000" },
+]; //maybe add 1000+
 
 const ratings = [
     { name: "4 stars and up", rating: 4 },
     { name: "3 stars and up", rating: 3 },
     { name: "2 stars and up", rating: 2 },
-    { name: "1 stars and up", rating: 1 }]
+    { name: "1 star and up", rating: 1 },
+];
+
 const Search = () => {
     const [categories, setCategories] = useState([]);
     const navigate = useNavigate();
     const { search } = useLocation();
-
     const searchParams = new URLSearchParams(search);
-    const category = searchParams.get('category') || 'all';
-    const query = searchParams.get('query') || 'all';
-    const price = searchParams.get('price') || 'all';
-    const rating = searchParams.get('rating') || 'all';
-    const order = searchParams.get('order') || 'newest';
-    const page = searchParams.get('page') || 1;
+    const category = searchParams.get("category") || "all";
+    const query = searchParams.get("query") || "all";
+    const price = searchParams.get("price") || "all";
+    const rating = searchParams.get("rating") || "all";
+    const order = searchParams.get("order") || "newest";
+    const page = searchParams.get("page") || 1; //feels smelly...
 
-    const [{ loading, error, products, pages, countProducts }, dispatch] = useReducer(searchPageReducer, { loading: true, error: "" });
+    const [{ loading, error, products, pages, countProducts }, dispatch] =
+        useReducer(searchPageReducer, { loading: true, error: "" });
 
     useEffect(() => {
         const getCategories = async () => {
             try {
-                console.log("Hi");
-                const { data } = await axios.get('/api/v1/product/categories');
+                const { data } = await axios.get("/api/v1/product/categories");
                 setCategories(data);
             } catch (error) {
-                toast.error(getError(error))
+                toast.error(getError(error));
             }
         };
         getCategories();
@@ -62,9 +69,163 @@ const Search = () => {
 
     return (
         <div>
-            Hi
-        </div>
-    )
-}
+            <Title title="Search Page" />
+            <Row>
+                <Col md={3}>
+                    <h3>Categories:</h3>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link
+                                    className={"all" === category ? "text-bold" : ""} //doesn't work
+                                    to={getFilterURI(search, { category: "all" })}
+                                >
+                                    Any
+                                </Link>
+                            </li>
+                            {categories.map((categoryLocal) => (
+                                <li key={categoryLocal}>
+                                    <Link
+                                        className={categoryLocal === category ? "text-bold" : ""}
+                                        to={getFilterURI(search, { category: categoryLocal })}
+                                    >
+                                        {categoryLocal}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <h3>Price:</h3>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link
+                                    className={"all" === price ? "text-bold" : ""} //doesn't work
+                                    to={getFilterURI(search, { price: "all" })}
+                                >
+                                    Any
+                                </Link>
+                            </li>
+                            {prices.map((priceLocal) => (
+                                <li key={priceLocal.value}>
+                                    <Link
+                                        className={priceLocal.value === price ? "text-bold" : ""}
+                                        to={getFilterURI(search, { price: priceLocal.value })}
+                                    >
+                                        {priceLocal.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <h3>Reviews:</h3>
+                    <div>
+                        <ul>
+                            {ratings.map((ratingLocal) => (
+                                <li key={ratingLocal.rating}>
+                                    <Link
+                                        className={ratingLocal.rating === rating ? "text-bold" : ""}
+                                        to={getFilterURI(search, { rating: ratingLocal.rating })}
+                                    >
+                                        <Rating rating={ratingLocal.rating} caption={" "} />
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </Col>
+                <Col md={9}>
+                    {loading ? (
+                        <Loading />
+                    ) : error ? (
+                        <MessageBox variant="danger">{error}</MessageBox>
+                    ) : (
+                        <>
+                            <Row>
+                                <Col md={6}>
+                                    <div>
+                                        {countProducts === 0 ? "No" : countProducts} Results
+                                        {query !== "all" && " for " + query}
+                                        {category !== "all" && " : " + category}
+                                        {price !== "all" && " : Price " + price}
+                                        {rating !== "all" && " : Rating " + rating + " & up"}
+                                        {query !== "all" ||
+                                            category !== "all" ||
+                                            price !== "all" ||
+                                            rating !== "all" ? (
+                                            <Button
+                                                variant="light"
+                                                onClick={() =>
+                                                    navigate(
+                                                        getFilterURI(search, {
+                                                            query: "all",
+                                                            category: "all",
+                                                            price: "all",
+                                                            rating: "all",
+                                                            order: "newest",
+                                                            page: 1,
+                                                        })
+                                                    )
+                                                }
+                                            >
+                                                <i className="fas fa-times-circle" />
+                                            </Button>
+                                        ) : null}
+                                    </div>
+                                </Col>
+                                <Col className="text-end">
+                                    Sort by{" "}
+                                    <select
+                                        value={order}
+                                        onChange={(e) => {
+                                            navigate(getFilterURI(search, { order: e.target.value }));
+                                        }}
+                                    >
+                                        <option value="newest">Newest Arrivals</option>
+                                        <option value="lowest">Price: Low to High</option>
+                                        <option value="highest">Price: High to Low</option>
+                                        <option value="toprated">Customer Reviews</option>
+                                    </select>
+                                </Col>
+                            </Row>
+                            {products.length === 0 && (
+                                <MessageBox>No Product Found</MessageBox>
+                            )}
+                            <Row>
+                                {products.map((product) => (
+                                    <Col sm={6} lg={4} className="mb-3" key={product._id}>
+                                        <Product product={product}></Product>
+                                    </Col>
+                                ))}
+                            </Row>
 
-export default Search
+                            <div>
+                                {[...Array(pages).keys()].map((x) => (
+                                    <LinkContainer
+                                        key={x + 1}
+                                        className="mx-1"
+                                        to={{
+                                            pathname: "/search",
+                                            search: getFilterURI(search, { page: x + 1 }, true),
+                                        }}
+                                    >
+                                        <Button
+                                            className={
+                                                Number(page) === x + 1 ? "highlight-current-page" : ""
+                                            }
+                                            variant="light"
+                                        >
+                                            {x + 1}
+                                        </Button>
+                                    </LinkContainer>
+                                ))}
+                            </div>
+                        </>
+                    )}
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default Search;
