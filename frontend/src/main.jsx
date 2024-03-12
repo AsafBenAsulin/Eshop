08a0b77369f1f82bf3e768bea612828ff5d6256c
AsafBenAsulin <156786762+AsafBenAsulin@users.noreply.github.com>
2024-03-12 11:52:51 +0200

    added.env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,9 @@ import { HelmetProvider } from 'react-helmet-async'
 import axios from 'axios'
 import {StoreProvider} from "./store.jsx"
 
-axios.defaults.baseURL = 'http://localhost:8080'
+
+const baseURL = import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL = baseURL;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
